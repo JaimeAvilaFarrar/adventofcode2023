80200c54a58cced2086cce31893453a5679b70a9
Jaime Avila Farrar <jaime.avila.farrar@upply.com>
2023-12-03 17:48:18 +0100

    Init + calendar day 01 part 01

---
+++ calendar/day01/code/part01.py
@@ -0,0 +1,16 @@
+import re
+
+with open("C:/Users/jaime/VSCodeProjects/Git/adventofcode2023/calendar/day01/data/input.txt", "r") as file:
+    input = file.read()
+
+input_list = input.split("\n")
+
+input_single_digits_list_of_list = [re.findall(r'\d', input_value) for input_value in input_list]
+
+input_2_digits_list = [
+    int(f"{l[0]}{l[-1]}") if len(l) > 1
+    else int(f"{l[0]}{l[0]}")
+    for l in input_single_digits_list_of_list
+]
+
+result = sum(input_2_digits_list)
